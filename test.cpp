5d2a1b6681b4d99cbdd18bd65df43779c88f076e
Yujie Guo <3196813180@qq.com>
2022-11-15 17:13:52 +0800

    module

---
+++ test.cpp
@@ -0,0 +1,32 @@
+/* VScode_Version */
+
+#include <iostream>
+//#include <sstream>
+//#include <cmath>
+//#include <bitset>
+//#include <iomanip>
+//#include <cstring>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//#include <list>
+//#include <queue>
+//#include <stack>
+//#include <set>
+//#include <map>
+
+#define RG register
+#define LL long long
+#define ULL unsigned long long
+#define INF 0x7fffffff
+using namespace std;
+
+int main()
+{
+	
+
+
+
+    system("pause");
+	return 0;
+}
